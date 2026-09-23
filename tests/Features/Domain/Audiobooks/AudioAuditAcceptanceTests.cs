9cbc3a39ba312810701ca521f7bd05afc82a7e7d
Nexal <nexalapp@gmail.com>
2026-09-23 10:43:07 -0600

    chore: drop an unused using the format check flags

--- tests/Features/Domain/Audiobooks/AudioAuditAcceptanceTests.cs
+++ tests/Features/Domain/Audiobooks/AudioAuditAcceptanceTests.cs
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-using Listenarr.Domain.Audiobooks;
 using Listenarr.Domain.Audiobooks.Audit;
 using Listenarr.Tests.Common;
 
